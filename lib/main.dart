ba3bcc35ffdbfd1dfa3529304f44cfeee69241f2
Esben Viskum <viskum@viskum.dk>
2021-11-05 13:00:23 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() {
+  runApp(const Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [
+    // Icon(Icons.check, color: Colors.green),
+    // Icon(Icons.close, color: Colors.red),
+    // Icon(Icons.close, color: Colors.red),
+    // Icon(Icons.close, color: Colors.red),
+    // Icon(Icons.close, color: Colors.red),
+  ];
+
+  List<Map<String, dynamic>> questions = [
+    {'question': 'You can lead a cow down stairs but not up stairs.', 'answer': false},
+    {'question': 'Approximately one quarter of human bones are in the feet.', 'answer': true},
+    {'question': 'A slug\'s blood is green.', 'answer': true}
+  ];
+
+  int currentQuestion = 0;
+
+  void _evaluateAnswer(bool answer) {
+    setState(() {
+      if ((questions[currentQuestion]['answer'] as bool) == answer)
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+      else
+        scoreKeeper.add(Icon(Icons.cancel, color: Colors.red));
+      if (currentQuestion == 2) {
+        Alert(
+          context: context,
+          title: "The end",
+          desc: "You've answered all questions. We'll restart.",
+        ).show();
+        currentQuestion = 0;
+      } else
+        currentQuestion++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                // 'This is where the question text will go.',
+                questions[currentQuestion]['question'] as String,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Colors.green, onPrimary: Colors.white),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  // color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                _evaluateAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                _evaluateAnswer(false);
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
